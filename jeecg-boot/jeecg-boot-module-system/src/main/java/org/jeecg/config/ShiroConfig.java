bc48a914dd6a9dee7685a8ed4d679cb13ffeb178
heng jiang <>
2019-10-31 00:00:58 -0700

    add config

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/ShiroConfig.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/ShiroConfig.java
@@ -70,6 +70,8 @@ public class ShiroConfig {
 		filterChainDefinitionMap.put("/cas/client/validateLogin", "anon");
 		// 配置不会被拦截的链接 顺序判断
 		filterChainDefinitionMap.put("/sys/getCheckCode", "anon"); //登录验证码接口排除
+		filterChainDefinitionMap.put("/sys/mobileWxLogin", "anon");
+		filterChainDefinitionMap.put("/sys/miniLogin", "anon");
 		filterChainDefinitionMap.put("/sys/login", "anon"); //登录接口排除
 		filterChainDefinitionMap.put("/sys/mLogin", "anon"); //登录接口排除
 		filterChainDefinitionMap.put("/sys/logout", "anon"); //登出接口排除
