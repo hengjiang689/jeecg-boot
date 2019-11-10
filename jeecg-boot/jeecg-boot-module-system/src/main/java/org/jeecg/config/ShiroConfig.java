545243b982148a2bb7f59720b2633532f8df2f21
heng jiang <>
2019-11-09 22:08:28 -0800

    update queryById

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/ShiroConfig.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/ShiroConfig.java
@@ -100,6 +100,10 @@ public class ShiroConfig {
 		filterChainDefinitionMap.put("/**/*.png", "anon");
 		filterChainDefinitionMap.put("/**/*.ico", "anon");
 
+		filterChainDefinitionMap.put("/carousel/wbCarousel/list", "anon");
+		filterChainDefinitionMap.put("/carousel/wbCarousel/queryById", "anon");
+		filterChainDefinitionMap.put("/sys/getPhoneSmsCode", "anon");
+		
 		// update-begin--Author:sunjianlei Date:20190813 for：排除字体格式的后缀
 		filterChainDefinitionMap.put("/**/*.ttf", "anon");
 		filterChainDefinitionMap.put("/**/*.woff", "anon");
