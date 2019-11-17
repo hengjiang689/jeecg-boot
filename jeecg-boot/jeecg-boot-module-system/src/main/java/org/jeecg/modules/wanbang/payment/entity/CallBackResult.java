e9879c9fa8d8484f21c9c3d80a3019dd6e90faaa
heng jiang <>
2019-11-16 21:12:35 -0800

    add xml

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/wanbang/payment/entity/CallBackResult.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/wanbang/payment/entity/CallBackResult.java
@@ -4,6 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(
+        name = "xml"
+)
+@XmlAccessorType(XmlAccessType.FIELD)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
